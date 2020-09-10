680ef4661f2005c6da18f5a9d31e6b490b8d8302
Diego Carrillo Sandrock <diegocarrillosandrock@gmail.com>
2020-09-10 11:41:39 -0300

    first commit

---
+++ nasa_request2.rb
@@ -0,0 +1,12 @@
+require "uri"
+require "net/http"
+
+url = URI("https://api.nasa.gov/planetary/apod?api_key=9zeOm3k53oc9dW1ZUrFK1EqlYzc9sPWkqSBAaPr4")
+
+https = Net::HTTP.new(url.host, url.port);
+https.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+
+response = https.request(request)
+puts response.read_body
---
+++ opportunity.rb
@@ -0,0 +1,12 @@
+require "uri"
+require "net/http"
+
+url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=1000&api_key=DEMO_KEY")
+
+https = Net::HTTP.new(url.host, url.port);
+https.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+
+response = https.request(request)
+puts response.read_body
