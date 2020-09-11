dbe6ae2e143418913cd560e013726db660ef944f
Diego Carrillo Sandrock <diegocarrillosandrock@gmail.com>
2020-09-11 10:06:22 -0300

    img added

--- probando.rb
+++ probando.rb
@@ -1,18 +1,2 @@
-require 'uri'
-
-def request(url1, api_key)
-    c= "#{url1}#{api_key}"
-    puts c
-
-
-end
-
-#request(blabbermouth, advertise)
-
-def url_with_api_key(url, api_key)
-    "#{url}api_key=#{api_key}"
-  
-end
-  
-d =url_with_api_key 'https://example.com', 'foobarbaz'
-puts d
+r_curiosity["photos"].each do |i|
+    puts i["img_src"]
--- prueba.rb
+++ prueba.rb
@@ -13,15 +13,36 @@ def request(url1, api_key)
     request = Net::HTTP::Get.new(url)
 
     response = https.request(request)
-    puts response.read_body  
+    body = JSON.parse response.read_body
+    return(body)
 end
+=begin
+def buid_web_page(hash)
+    hash.["photos":[{[0].each do |i|
+        puts i["img_src"]
+    end
+end
+=end
 
 
 url_curiosity = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&"
 url_opportunity = "https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=1000&"
 url_spirit = "https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=1000&"
-api_key = "DEMO_KEY"
+api_key = "9zeOm3k53oc9dW1ZUrFK1EqlYzc9sPWkqSBAaPr4"
 
-request(url_curiosity, api_key)
-#request(url_opportunity, api_key)
-#request(url_spirit, api_key)
+r_curiosity = request(url_curiosity, api_key)
+r_opportunity = request(url_opportunity, api_key)
+r_spirit = request(url_spirit, api_key)
+
+r_curiosity["photos"].each do |i|
+    puts i["img_src"]
+end
+r_opportunity["photos"].each do |i|
+    puts i["img_src"]
+end
+r_spirit["photos"].each do |i|
+    puts i["img_src"]
+end
+
+
+#buid_web_page(r_curiosity)
