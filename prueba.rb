3d20988ee57d0da4bc3fc87ac571a1c7b6eb8ae2
Diego Carrillo Sandrock <diegocarrillosandrock@gmail.com>
2020-09-11 18:20:32 -0300

    last commit

--- prueba.rb
+++ prueba.rb
@@ -71,20 +71,38 @@ merge_final = merge1.merge(spirit_invert)
 all_photos = merge_final.invert
 
 
+def buid_web_page(hash1)
+    head = '<!doctype html>
+        <html lang="en">
+        <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-=begin
-def buid_web_page(hash)
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    </head>
-    <body>
-    
-    </body>
-    </html>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        </head>
+        <body>
+        <ul>'
+
+        body = ""
+        hash1.each do |key, value|
+            body+= "<li><img src=\'#{value}\'></li>"
+        end
+
+        foot = '</ul>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        </body>
+    </html>'
+    total = head+body+foot
+    File.write('./index.html', total)
+    return total
 end
-=end
-#buid_web_page(r_curiosity)
+
+buid_web_page(all_photos)
+
